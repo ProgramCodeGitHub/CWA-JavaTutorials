8321ab94e007fd2f6214122e43242df28b865e1a
ProgramCodeGitHub <akshat762002@gmail.com>
2021-05-14 13:14:47 +0530

    JavaTutorial-1

---
+++ Practical1.java
@@ -0,0 +1,13 @@
+/*
+Practical 1 : Write a Program that displays Welcome to Java,
+Learning Java Now and Programming is fun.
+ */
+package com.company;
+
+public class Practical1 {
+    public static void main(String args[])
+    {
+        System.out.println("Welcome to Java, \n" +
+                "Learning Java Now and Programming is fun.");
+    }
+}
